d95cc7b032d29df7d12c57983663be5435fcf40d
Justin Schwartzenberger <schwarty@gmail.com>
2016-05-15 09:51:17 -0700

    remove menu items service to reduce complexity

--- src/app/counter/order-sheet/order-sheet.component.ts
+++ src/app/counter/order-sheet/order-sheet.component.ts
@@ -1,21 +1,14 @@
 import {Component} from '@angular/core';
-import {MenuService} from '../shared';
 import {DebugPanelComponent} from '../debug-panel/debug-panel.component';
 
 @Component({
   selector: 'order-sheet',
-  providers: [MenuService],
   directives: [DebugPanelComponent],
   templateUrl: 'order-sheet.component.html',
   styleUrls: ['order-sheet.component.css'],
   moduleId: module.id
 })
 export class OrderSheetComponent {
-  menuItems = {};
-  
-  constructor(private _menuService: MenuService) {}
-
-  ngOnInit() {
-    this.menuItems = this._menuService.getMenuItems();
+    constructor() {
   }
 }
