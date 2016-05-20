a0e2d08de330843fac28942b209a961e77740b18
Justin Schwartzenberger <schwarty@gmail.com>
2016-05-20 11:11:40 -0700

    end of video commit

--- src/app/counter/order-sheet/order-sheet.component.ts
+++ src/app/counter/order-sheet/order-sheet.component.ts
@@ -20,7 +20,22 @@ export class OrderSheetComponent {
     this.orderSheetForm = this._formBuilder.group({
       customerName: this._formBuilder.control(null),
       specialtySandwich: this._formBuilder.control(null),
-      otherNotes: this._formBuilder.control(null)
+      otherNotes: this._formBuilder.control(null),
+      meats: this._formBuilder.group({
+        meatHam: this._formBuilder.control(null),
+        meatTurkey: this._formBuilder.control(null),
+        meatRoastBeef: this._formBuilder.control(null)
+      }),
+      cheeses: this._formBuilder.group({
+        cheeseProvolone: this._formBuilder.control(null),
+        cheeseCheddar: this._formBuilder.control(null),
+        cheeseSwiss: this._formBuilder.control(null)
+      }),
+      veggiesAndSuch: this._formBuilder.group({
+        veggieLettuce: this._formBuilder.control(null),
+        veggieTomato: this._formBuilder.control(null),
+        veggieMustard: this._formBuilder.control(null)
+      }),
     });
   }
 }
