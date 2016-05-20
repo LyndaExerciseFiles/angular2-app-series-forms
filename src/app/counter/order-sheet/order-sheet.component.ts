2cc2bd77083df3290b13ed2fc607fc34e985c2b2
Justin Schwartzenberger <schwarty@gmail.com>
2016-05-20 12:24:36 -0700

    end of video commit

--- src/app/counter/order-sheet/order-sheet.component.ts
+++ src/app/counter/order-sheet/order-sheet.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {ControlGroup, Control, RadioButtonState, FormBuilder} from '@angular/common';
+import {ControlGroup, Control, ControlArray, RadioButtonState, FormBuilder} from '@angular/common';
 import {DebugPanelComponent} from '../debug-panel/debug-panel.component';
 
 @Component({
@@ -11,6 +11,7 @@ import {DebugPanelComponent} from '../debug-panel/debug-panel.component';
 })
 export class OrderSheetComponent {
   orderSheetForm: ControlGroup;
+  weirdRequestsControls: ControlArray;
   
   constructor(private _formBuilder: FormBuilder) {
     this._buildForm();
@@ -24,7 +25,6 @@ export class OrderSheetComponent {
         sizeLarge: this._formBuilder.control(new RadioButtonState(false, 'large'))
       }),
       specialtySandwich: this._formBuilder.control(null),
-      otherNotes: this._formBuilder.control(null),
       breads: this._formBuilder.group({
         breadWheatRoll: this._formBuilder.control(new RadioButtonState(false, 'wheat roll')),
         breadWhiteRoll: this._formBuilder.control(new RadioButtonState(false, 'white roll')),
@@ -44,7 +44,21 @@ export class OrderSheetComponent {
         veggieLettuce: this._formBuilder.control(null),
         veggieTomato: this._formBuilder.control(null),
         veggieMustard: this._formBuilder.control(null)
-      })
+      }),
+      weirdRequests: this._formBuilder.array([
+        this._formBuilder.control(null)
+      ]),
+      otherNotes: this._formBuilder.control(null)
     });
+    
+    this.weirdRequestsControls = this.orderSheetForm.find('weirdRequests') as ControlArray;
+  }
+  
+  onAddWeirdRequest() {
+    this.weirdRequestsControls.push(this._formBuilder.control(null));
+  }
+  
+  onRemoveWeirdRequest(index) {
+    this.weirdRequestsControls.removeAt(index);
   }
 }
