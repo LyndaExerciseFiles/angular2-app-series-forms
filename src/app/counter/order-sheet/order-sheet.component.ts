a16042d06596e875a1b2db364153912340ca1be0
Justin Schwartzenberger <schwarty@gmail.com>
2016-05-20 11:40:30 -0700

    end of video commit

--- src/app/counter/order-sheet/order-sheet.component.ts
+++ src/app/counter/order-sheet/order-sheet.component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {ControlGroup, Control, FormBuilder} from '@angular/common';
+import {ControlGroup, Control, RadioButtonState, FormBuilder} from '@angular/common';
 import {DebugPanelComponent} from '../debug-panel/debug-panel.component';
 
 @Component({
@@ -19,8 +19,17 @@ export class OrderSheetComponent {
   private _buildForm() {
     this.orderSheetForm = this._formBuilder.group({
       customerName: this._formBuilder.control(null),
+      sizes: this._formBuilder.group({
+        sizeSmall: this._formBuilder.control(new RadioButtonState(false, 'small')),
+        sizeLarge: this._formBuilder.control(new RadioButtonState(false, 'large'))
+      }),
       specialtySandwich: this._formBuilder.control(null),
       otherNotes: this._formBuilder.control(null),
+      breads: this._formBuilder.group({
+        breadWheatRoll: this._formBuilder.control(new RadioButtonState(false, 'wheat roll')),
+        breadWhiteRoll: this._formBuilder.control(new RadioButtonState(false, 'white roll')),
+        breadSourdoughRoll: this._formBuilder.control(new RadioButtonState(false, 'sourdough roll'))
+      }),
       meats: this._formBuilder.group({
         meatHam: this._formBuilder.control(null),
         meatTurkey: this._formBuilder.control(null),
@@ -35,7 +44,7 @@ export class OrderSheetComponent {
         veggieLettuce: this._formBuilder.control(null),
         veggieTomato: this._formBuilder.control(null),
         veggieMustard: this._formBuilder.control(null)
-      }),
+      })
     });
   }
 }
